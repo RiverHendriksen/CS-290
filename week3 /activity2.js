4d836474bbb6521317076549a46ea3441ceef6d3
River <riverRiver@MU_IT.mu.oregonstate.edu>
2016-04-11 10:05:18 -0700

    yas

---
+++ week3 /activity2.js
@@ -0,0 +1,11 @@
+output1();
+
+function output1(){
+	console.log("i sure hope this works, also this is the first part") 
+}
+
+output2();
+
+var output2 = function (){
+	console.log("i sure hope this doesnt work, also this is the second part")
+}
