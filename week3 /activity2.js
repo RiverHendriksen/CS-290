2f51f02df3f0f2d329ce622bbb74964de0c1b6c6
RiverHendriksen <hendriri@oregonstate.edu>
2016-04-16 16:31:49 -0700

    just trying it all out

--- week3 /activity2.js
+++ week3 /activity2.js
@@ -4,8 +4,11 @@ function output1(){
 	console.log("i sure hope this works, also this is the first part") 
 }
 
+//output: i sure hope this works, also this is the first part
 output2();
 
 var output2 = function (){
 	console.log("i sure hope this doesnt work, also this is the second part")
 }
+
+//output: TypeError: output2 is not a function
