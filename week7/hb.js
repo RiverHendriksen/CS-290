d8596d03593482712f8875be797fd2c2d700ee5f
Ubuntu <ubuntu@ip-172-31-44-106.us-west-2.compute.internal>
2016-05-16 04:02:59 +0000

    we done

--- week7/hb.js
+++ week7/hb.js
@@ -3,33 +3,64 @@ var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 var bodyParser = require('body-parser');
-
+var context = {}
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 3000);
+app.set('port', 3050);
 
 app.get('/',function(req,res){
   res.render('home') 
 });
 
-app.get('/get-loopback-improved',function(req,res){
+function data(x, req){
+ var different_route_array = []; 
+ context = {};
+ if(x == "query"){
+    for (var p in req.query){
+    	different_route_array.push({'name':p,'value':req.query[p]})
+    	console.log(req.query[p]);
+  	}
+    context.type = "GET";
+ }
+  if(x == "body"){
+    for (var p in req.body){
+        different_route_array.push({'name':p,'value':req.body[p]})
+        console.log(req.body[p]);
+        }
+    for (var p in req.query){
+        different_route_array.push({'name':p,'value':req.query[p]})
+        console.log(req.query[p]);
+        }
+    context.type = "POST";
+ }
+  console.log(different_route_array);
+  
+  //create a data list that is of the array
+  context.dataList = different_route_array;
+}
+
+app.get('/',function(req,res){
+  res.render('home') 
+});
+
+app.get('/postreq',function(req,res){
   //create an array
-  var qParams = [];
+  data( "query", req);
   //iterate through the for loop and push to vals to the array at a single index
-  for (var p in req.query){
+  /*for (var p in req.query){
     qParams.push({'name':p,'value':req.query[p]})
   }
   var context = {};
-  //create a data list that is of the array
-  context.dataList = qParams;
-  res.render('get-loopback-improved', context);
+  context.dataList = qParams;*/
+  res.render('postreq', context);
 });
 
-app.post('/post-loopback', function(req,res){
-  var qParams = [];
+app.post('/postreq', function(req,res){
+  data("body",req);
+  /*var qParams = [];
   for (var p in req.body){
     qParams.push({'name':p,'value':req.body[p]})
   }
@@ -37,8 +68,10 @@ app.post('/post-loopback', function(req,res){
   console.log(req.body);
   var context = {};
   context.dataList = qParams;
-  res.render('post-loopback', context);
+  context.type = "POST";*/
+  res.render('postreq', context);
 });
+
 
 app.use(function(req,res){
   res.status(404);
