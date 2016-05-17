cd33cd2dc1bd9e7229b398abe4e0f54cddae84bd
RivH <hendriri@oregonstate.edu>
2016-05-17 10:31:17 -0700

    Update anime.js

--- how to/anime.js
+++ how to/anime.js
@@ -3,14 +3,14 @@ var req = new XMLHttpRequest();
 //var payload = {dawg:null};
 //payload.xd = document.getElementById(specificdog).value;
 //console.log(payload.xd)
-req.open('GET', 'http://hummingbird.me/api/v1/search/anime?query=["persona"]', true);
+req.open('GET', 'http://pokeapi.co/api/v2/pokemon/1/', true);
 req.addEventListener('load',function(){
 if(req.status >= 200 && req.status < 400){
      console.log(JSON.parse(req.responseText));
      var response = JSON.parse(req.responseText);
-     document.getElementById('Ucity').textContent = response.slug;
-     document.getElementById('humidity').textContent = response.synposis;
-     document.getElementById('tempbroture').textContent = response.episode_length;
+     document.getElementById('Pokid').textContent = response.slug;
+     document.getElementById('Pname').textContent = response.synposis;
+     //document.getElementById('tempbroture').textContent = response.episode_length;
 } else {
      console.log("Error in network request: " + request.statusText);
      }});
