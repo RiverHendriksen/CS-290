0b267fe94f9778f01b5898e7cff4f61b4d381f0c
RiverHendriksen <hendriri@oregonstate.edu>
2016-05-22 17:44:18 -0700

    new code

---
+++ week8/main.js
@@ -0,0 +1,64 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+var session = require('express-sessions');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
+app.use(session({secret:'SuperSecretPassword'}));
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3050);
+
+
+//////////////////////////////////////////////////////////////
+app.get('/',function(req,res,next){
+  var context = {};
+  //If there is no session, go to the main page.
+  if(!req.session.name){
+    res.render('login', context);
+    return;
+  }
+  context.name = req.session.name;
+  context.toDoCount = req.session.toDo.length || 0;
+  context.toDo = req.session.toDo || [];
+  console.log(context.toDo);
+  res.render('main',context);
+});
+
+//////////////////////////////////////////////////////////////
+app.post('/',function(req,res){
+  var context = {};
+
+  if(req.body['New List']){
+    req.session.name = req.body.name;
+    req.session.toDo = [];
+    req.session.curId = 0;
+  }
+
+  //If there is no session, go to the main page.
+  if(!req.session.name){
+    res.render('login', context);
+    return;
+  }
+
+  if(req.body['Add Item']){
+    req.session.toDo.push({"name":req.body.name, "id":req.session.curId});
+    req.session.curId++;
+  }
+
+  if(req.body['Done']){
+    req.session.toDo = req.session.toDo.filter(function(e){
+      return e.id != req.body.id;
+    })
+  }
+
+  context.name = req.session.name;
+  context.toDoCount = req.session.toDo.length;
+  context.toDo = req.session.toDo;
+  console.log(context.toDo);
+  res.render('main',context);
+});
